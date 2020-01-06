8ef72c9eb15374050ce4f3cf294d580c803228fb
Josh Hawthorne <JoHawth31@gmail.com>
2020-01-06 14:35:44 -0800

    Work up to page 70

--- chat/auth.go
+++ chat/auth.go
@@ -17,15 +17,12 @@ type authHandler struct {
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	_, err := r.Cookie("auth")
-	if err == http.ErrNoCookie {
+	if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
 		//not authenticated
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 		return
-	}
-
-	if err != nil {
+	} else if err != nil {
 		//some other error
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -91,7 +88,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		authCookieValue := objx.New(map[string]interface{}{
-			"name": user.Name(),
+			"name":       user.Name(),
+			"avatar_url": user.AvatarURL(),
 		}).MustBase64()
 
 		http.SetCookie(w, &http.Cookie{
--- chat/room.go
+++ chat/room.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/stretchr/objx"
+
 	"github.com/QO-Development/blueprints/trace"
 	"github.com/gorilla/websocket"
 )
@@ -17,7 +19,7 @@ var upgrader = &websocket.Upgrader{ReadBufferSize: sockerBufferSize, WriteBuffer
 
 type room struct {
 	//forward is a channel that holds incoming messages that should be forwarded to other clients
-	forward chan []byte
+	forward chan *message
 	//join is a channel for clients wishing to join the room
 	join chan *client
 	//leave is a channel for clients wishing to leave the room
@@ -31,7 +33,7 @@ type room struct {
 //newRoom makes a new room
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -53,7 +55,7 @@ func (r *room) run() {
 			r.tracer.Trace("Client left")
 		case msg := <-r.forward:
 			//forward message to all clients
-			r.tracer.Trace("Message Received: ", string(msg))
+			r.tracer.Trace("Message Received: ", msg.Message)
 			for client := range r.clients {
 				client.send <- msg
 				r.tracer.Trace(" -- sent to client")
@@ -70,16 +72,21 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Failed to get auth cookie: ", err)
+		return
+	}
+
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 
 	r.join <- client
-
 	defer func() { r.leave <- client }()
-
 	go client.write()
 	client.read()
 }
